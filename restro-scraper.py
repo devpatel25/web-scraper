f3e3352193ac751dcc366889ca3532400df5734d
Dev Patel <88455065+devpatel25@users.noreply.github.com>
2023-12-30 23:27:02 +0530

    Add files via upload

---
+++ restro-scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url="https://www.yelp.com/search?find_desc=Restaurants&find_loc=Toronto%2C+Ontario%2C+Canada"
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+with open("restaurants.csv",'w',newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Name","Rating"])
+
+try:
+    responce= requests.get(url,headers=headers)
+    responce.raise_for_status()
+except requests.exceptions.HTTPError as errh:
+    print ("Http Error:",errh)
+
+
+soup =BeautifulSoup(responce.text,"html.parser")
+
+restaurants=soup.find_all('li',class_="css-1qn0b6x")
+for restaurant in restaurants:
+    name=restaurant.find('a',class_='css-19v1rkv').text.strip()
+    rating=restaurant.find('span',class_=" css-gutk1c").text.strip()
+    print(f"Name: {name}\nRating: {range}\n")
+    writer.writerow([name,rating])
